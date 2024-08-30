10f906cb4fbfad8faa0b02d9f394b091193ea275
yangwenxi <yangwenxi@tanzhe.com>
2024-08-30 15:46:47 +0800

    add experiments setting

--- experiments/experiments_dbpedia.sh
+++ experiments/experiments_dbpedia.sh
@@ -1,6 +1,6 @@
 BATCH_SIZE=2000
 EMBEDDING_DIM=100
-NUM_EPOCHS=1000
+NUM_EPOCHS=600
 NUM_EPOCHS_PEP=300
 BETA=0.8
 MARGIN_KGE=6.0
@@ -8,8 +8,9 @@ MARGIN_HIER=6.0
 MODEL_NAME="TransE"
 LEARNING_RATE=0.001
 PEP="global"
+DATA="dbpedia"
 
-echo " python ./train.py --data_dir data/yago --batch_size $BATCH_SIZE  --embedding_dim $EMBEDDING_DIM \
+echo " python ./train.py --data_dir data/$DATA --batch_size $BATCH_SIZE  --embedding_dim $EMBEDDING_DIM \
     --num_epochs $NUM_EPOCHS \
     --learning_rate $LEARNING_RATE  \
     --beta $BETA \
@@ -18,7 +19,7 @@ echo " python ./train.py --data_dir data/yago --batch_size $BATCH_SIZE  --embedd
     --model_name $MODEL_NAME \
     --pep $PEP "
 
-python ./train.py --data_dir data/yago --batch_size $BATCH_SIZE  --embedding_dim $EMBEDDING_DIM \
+python ./train.py --data_dir data/$DATA --batch_size $BATCH_SIZE  --embedding_dim $EMBEDDING_DIM \
     --num_epochs $NUM_EPOCHS \
     --learning_rate $LEARNING_RATE  \
     --beta $BETA \
